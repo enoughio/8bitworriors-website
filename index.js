3ae23dfdde0097b12e2c89b6e7335cbf29b29463
Aman Kumar Verma <amritkumarverma848@gmail.com>
2024-12-18 22:46:59 +0530

    second commit

--- index.js
+++ index.js
@@ -8,3 +8,43 @@ blurs.style.left = dets.x - 150 + "px";
 blurs.style.top = dets.y - 150 + "px";
 });
 
+// index.js
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", function() {
+  let pages = gsap.utils.toArray('#page');  // Selecting elements with class 'page'
+
+  pages.forEach((page, i) => {
+    if (i !== pages.length) {
+      ScrollTrigger.create({
+        trigger: page,
+        pin: true,
+        pinSpacing: false,
+        start: 'top top',
+        end: () => `+=${page.clientHeight}`
+      });
+    }
+  });
+});
+
+$(document).ready(function() {
+  var SwiperSliderOne = new Swiper('.photography-swiper--slider', {
+            loop: true,
+            parallax: true,
+            autoplay: {
+                delay: 5000,
+            },
+            effect: 'fade',
+            autoHeight: true,
+            speed: 2500,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+                clickable: true,
+            },
+        });
+});
